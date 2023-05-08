594b12c6cb54364029faaa9927d67b010633a01d
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-08 16:28:37 +0430

    ADD SOME VARIABLE

--- src/components/appointment/appointment.js
+++ src/components/appointment/appointment.js
@@ -3,3 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createAppointment } from '../../redux/appointment/appointment';
 
 
+const BookAppointment = () => {
+    const dispatch = useDispatch();
+    const doctor = useSelector((state) => state.doctorReducer);
+    const { userid } = JSON.parse(window.localStorage.getItem('BookDoctorUser'));
+    const userID = userid || 1;
+    const { payload } = doctor;
+
+}
