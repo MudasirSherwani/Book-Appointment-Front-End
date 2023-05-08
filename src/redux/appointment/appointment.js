01fb902a6876cbaa175918e10db3eba0fd4a56de
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-08 16:08:03 +0430

    ADD REDUCER FOR APPIONTMENT

---
+++ src/redux/appointment/appointment.js
@@ -0,0 +1,25 @@
+const CREATE_APPOINTMENT = 'CREATE_APPOINTMENT';
+
+export const createNewAppointment = async (userid, datareserve) => {
+    const { disease, city, appointment_date, appointment_time } = datareserve;
+    const UserId = `${userid}`;
+    const reservation = {
+      disease,
+      city,
+      appointment_date,
+      appointment_time,
+      user: UserId,
+    };
+    const resp = await fetch(`http://127.0.0.1:3000/users/${userid}/appointments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        reservation,
+      }),
+    });
+    const data = await resp.text();
+    return data;
+  };
+
