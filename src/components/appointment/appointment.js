0b8a9049725309457ace5b5103d42b9b8dd7efe2
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-08 16:36:33 +0430

    ADD NEW APPIONTMENT FUNCTION

--- src/components/appointment/appointment.js
+++ src/components/appointment/appointment.js
@@ -5,9 +5,26 @@ import { createAppointment } from '../../redux/appointment/appointment';
 
 const BookAppointment = () => {
     const dispatch = useDispatch();
-    const doctor = useSelector((state) => state.doctorReducer);
+    const doctordata = useSelector((state) => state.doctorReducer);
     const { userid } = JSON.parse(window.localStorage.getItem('BookDoctorUser'));
     const userID = userid || 1;
-    const { payload } = doctor;
+    const { payload } = doctordata;
+    const doctorID = payload?.doctor.id;
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
 
-}
+    const newAppointment = (e) => {
+        e.preventDefault();
+        if (date === '') return
+        const newUser = {
+            date,
+            doctorID,
+            time,
+        };
+        dispatch(createAppointment(userID, newUser));
+        setDate('');
+        setTime('');
+    };
+
+
+};
