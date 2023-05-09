004f7da3fb89b8838785a5eb4a3a0143a0f3b0b9
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-09 16:28:01 +0100

    Use React Hooks to implement dynamic background

--- src/components/home/LandingPage.js
+++ src/components/home/LandingPage.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../../assets/logo.svg';
 import Button from '../button';
 import doc from '../../assets/backgrounds/doc_stet.jpg';
-// import book from '../../assets/backgrounds/book.jpg';
-// import consulting from '../../assets/backgrounds/consulting.jpg';
-// import help from '../../assets/backgrounds/help.jpg';
+import book from '../../assets/backgrounds/book.jpg';
+import consulting from '../../assets/backgrounds/consulting.jpg';
+import help from '../../assets/backgrounds/help.jpg';
 
-// const backgroundImages = [doc, book, consulting, help];
+const ImageChanger = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const backgroundImages = [doc, book, consulting, help];
 
-// const randomBackground = () => (
-//   // backgroundImages[Math.floor(Math.random() * backgroundImages.length)]
-// )
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((prev) => (prev + 1) % backgroundImages.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [backgroundImages.length]);
+
+  return (
+    <img
+      src={backgroundImages[imageIndex]}
+      alt="world vision clinic's logo"
+      className="main--bg"
+    />
+  );
+};
 
 const LandingPage = () => (
   <section className="landing--page">
-    <img src={doc} alt="doctor" className="landing--page--background" />
+    { ImageChanger()}
     <div className="welcome--page flex column">
-      <img src={Logo} alt="world vision clinic' logo" />
+      <img src={Logo} alt="world vision clinic' logo" className="main--logo" />
       <h1 className="main--header">World Vision Clinic</h1>
       <Button text="Book an Appointment" />
       <div className="main--slider column flex">
