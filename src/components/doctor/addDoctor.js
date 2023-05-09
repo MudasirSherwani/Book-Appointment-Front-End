1bb632febbdf2c0815a1c0b7dd04179ba43a8d54
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-09 15:45:38 +0430

    ADD INPUT FILED FOR NAME

--- src/components/doctor/addDoctor.js
+++ src/components/doctor/addDoctor.js
@@ -1,96 +1,115 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function AddDoctor(){
-    const user = JSON.parse(localStorage.getItem("user"));
-    const [name, setName] = useState("");
-    const [city, setCity] = useState("");
-    const [discription, setDiscription] = useState("");
-    const [image, setImage] = useState("");
-    const [speciality, setSpeciality] = useState("");
-    const [nameError, setNameError] = useState(false);
-    const [cityError, setCityError] = useState(false);
-    const [discriptionError, setDiscriptionError] = useState(false);
-    const [imageError, setImageError] = useState(false);
-    const [specialityError, setSpecialityError] = useState(false);
+function AddDoctor() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [discription, setDiscription] = useState('');
+  const [image, setImage] = useState('');
+  const [speciality, setSpeciality] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [cityError, setCityError] = useState(false);
+  const [discriptionError, setDiscriptionError] = useState(false);
+  const [imageError, setImageError] = useState(false);
+  const [specialityError, setSpecialityError] = useState(false);
 
-    const restValue = () => {
-        setName("");
-        setCity("");
-        setDiscription("");
-        setImage("");
-        setSpeciality("");
+  const restValue = () => {
+    setName('');
+    setCity('');
+    setDiscription('');
+    setImage('');
+    setSpeciality('');
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let errors = false;
+
+    if (name === '') {
+      setNameError(true);
+      errors = true;
+    } else {
+      setNameError(false);
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        let errors = false;
-
-        if(name === ""){
-            setNameError(true);
-            errors = true;
-        }else{
-            setNameError(false);
-        }
-
-        if(city === ""){
-            setCityError(true);
-            errors = true;
-        }else{
-            setCityError(false);
-        }
-
-        if(discription === ""){
-            setDiscriptionError(true);
-            errors = true;
-        }else{
-            setDiscriptionError(false);
-        }
-
-        if(image === ""){
-            setImageError(true);
-            errors = true;
-        }else{
-            setImageError(false);
-        }
-
-        if(speciality === ""){
-            setSpecialityError(true);
-            errors = true;
-        }else{
-            setSpecialityError(false);
-        }
-
-        if(!errors){
-            try{
-                const response = await fetch("http://localhost:3000/doctors", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-    
-                    },
-                    body: JSON.stringify({
-                        name,
-                        city,
-                        discription,
-                        image,
-                        speciality,
-                    }),
-                });
-                if(response.ok){
-                    toast.success('Doctor created successfully!');
-                    restValue();
-                }else{
-                    throw new Error('Failed to create doctor');
-                }
-            }
-            catch(error){
-                toast.error(error.message || 'Failed to create doctor');
-            }
-    
-        }
-    
+    if (city === '') {
+      setCityError(true);
+      errors = true;
+    } else {
+      setCityError(false);
     }
-    
-    
+
+    if (discription === '') {
+      setDiscriptionError(true);
+      errors = true;
+    } else {
+      setDiscriptionError(false);
+    }
+
+    if (image === '') {
+      setImageError(true);
+      errors = true;
+    } else {
+      setImageError(false);
+    }
+
+    if (speciality === '') {
+      setSpecialityError(true);
+      errors = true;
+    } else {
+      setSpecialityError(false);
+    }
+
+    if (!errors) {
+      try {
+        const response = await fetch('http://localhost:3000/doctors', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+
+          },
+          body: JSON.stringify({
+            name,
+            city,
+            discription,
+            image,
+            speciality,
+          }),
+        });
+        if (response.ok) {
+          toast.success('Doctor created successfully!');
+          restValue();
+        } else {
+          throw new Error('Failed to create doctor');
+        }
+      } catch (error) {
+        toast.error(error.message || 'Failed to create doctor');
+      }
+    }
+  };
+
+  return (
+    <section className="add-doctor">
+      <form onSubmit={handleSubmit}>
+        <h2>Add Doctor</h2>
+        <div className="form-group">
+            <span>Name:</span>
+            <input
+                type="text"
+                id="name-input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                datatestid = 'input-name'
+            />
+            {nameError && <span className="error">Name is required</span>}
+        </div>        
+
+
+      </form>
+
+    </section>
+  );
 }
+
+export default AddDoctor;
