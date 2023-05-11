0cf7a82953814a6ab9c45f7005281ee251d906ad
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2023-05-11 11:51:31 +0100

    Update Home.js
    Update styling for Homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import LandingPage from './LandingPage';
 
 const Home = () => (
-  <section className="home--container h--100">
-    <LandingPage />
-  </section>
+  <main className="main--container w-100 h--100"
+    <section className="home--container h--100">
+      <LandingPage />
+    </section>
+  </main>
 );
 
 export default Home;
