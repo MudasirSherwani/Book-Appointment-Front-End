94e601268049be10888458a846e67c8178670e22
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 00:10:51 +0300

    Fetch all doctors in doctorSlice

--- src/redux/doctor/doctorSlice.js
+++ src/redux/doctor/doctorSlice.js
@@ -3,3 +3,19 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import baseUrl from '../base_url';
 // actions
 const REMOVE_DOCTOR = "redux/doctor/remove";
+
+// show all doctors
+export const fetchDoctors = createAsyncThunk('GET_DOCTORS', async (thunkAPI) => {
+  const token = localStorage.getItem('token');
+  const requestOptions = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.get(`${baseUrl}doctors/`, requestOptions);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.error);
+  }
+});
