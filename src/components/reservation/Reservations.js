ad80025dc8cb65293ae8268595a1e6dc92813192
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-14 17:11:10 +0300

    Use redux in reservationsList feature

--- src/components/reservation/Reservations.js
+++ src/components/reservation/Reservations.js
@@ -1,45 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { getReservations } from '../../redux/reservation/reservationsSlice';
 import baseUrl from '../../redux/base_url';
 
 function Reservations() {
-  const [reserveList, setReserveList] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const userId = JSON.parse(window.localStorage.getItem('user_id'));
-
-    const fetchReservations = async () => {
-      try {
-        const response = await fetch(`${baseUrl}users/${userId}/appointments/`, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch reservation list');
-        }
-
-        const data = await response.json();
-        setReserveList(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchReservations();
-  }, []);
-
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state) => state.reservations);
+  // Fetch all doctors to get the doctor name
   const GetDoctorName = ({ doctorId }) => {
     const [doctor, setDoctor] = useState(null);
-
     useEffect(() => {
       const token = localStorage.getItem('token');
-
       const fetchDoctor = async () => {
         try {
           const response = await fetch(`${baseUrl}doctors/${doctorId}`, {
@@ -67,14 +39,21 @@ function Reservations() {
     return doctor ? doctor.name : '';
   };
 
-  const content = loading ? (
-    <div className="d-flex justify-content-center">
-      <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
+  useEffect(() => {
+    dispatch(getReservations());
+  }, [dispatch]);
+  let content;
+  if (loading === 'pending') {
+    content = (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
       </div>
-    </div>
-  ) : (
-    reserveList.map((reservation) => (
+    );
+  }
+  if (loading === 'idle') {
+    content = data.map((reservation) => (
       <div className="col-sm-6" key={reservation.id}>
         <div className="card">
           <div className="card-body">
@@ -82,13 +61,24 @@ function Reservations() {
               <GetDoctorName doctorId={reservation.doctor_id} />
             </h5>
             <h5 className="card-title">{reservation.city}</h5>
-            <p className="card-text">{reservation.appointment_date}</p>
+            <p className="card-text">
+              {' '}
+              {reservation.appointment_date}
+              {' '}
+            </p>
+
           </div>
         </div>
       </div>
-    ))
-  );
-
+    ));
+  }
+  if (error !== null) {
+    content = (
+      <div className="alert alert-danger" role="alert">
+        {error}
+      </div>
+    );
+  }
   return <div className="row">{content}</div>;
 }
 
