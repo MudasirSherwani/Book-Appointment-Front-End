128de5256c48531f40e2727d9f85ed699b39057a
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 00:22:09 +0300

    Add the doctorSlice to the combinedReducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -14,6 +14,7 @@ const initialState = {
 const rootReducer = combineReducers({
   auth: authReducer,
   reservations: reservationsReducer,
+  doctorSlice: doctorsReducer,
 });
 
 export default configureStore({
