67c6b799df67c855e888b2b4730d4bfca9886f3e
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 00:36:26 +0300

    Fix linters

--- src/components/doctor/DeleteDoctor.js
+++ src/components/doctor/DeleteDoctor.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
-import { removeDoctor } from "../../redux/doctor/doctorSlice";
+import { removeDoctor } from '../../redux/doctor/doctorSlice';
 import baseUrl from '../../redux/base_url';
 
 const DeleteDoctor = () => {
@@ -10,16 +10,16 @@ const DeleteDoctor = () => {
 
   useEffect(() => {
     // Fetch API data and update the state of doctors
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     fetch(`${baseUrl}doctors/`, {
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         return response.json();
       })
@@ -36,46 +36,59 @@ const DeleteDoctor = () => {
   };
 
   return (
-    <React.Fragment>
-    <Container className="content">
-   <div className="row">
-    <div className="col-sm-12">
-    <h2 className="mt-4 mb-4 fw-bold">
-        Delete a Doctor  
-      </h2>
-      <form className="form w-100">
-      <table className="table">
-      <thead>            
-      <tr>
-      <th>Sr. No</th>
-      <th>Doctor Name</th>
-      <th>City</th>
-      <th>speciality</th>
-      <th>Action</th>
-      </tr>
-      </thead>
-      <tbody>
-      {doctors.map((doctor,index) => ( 
-      <tr   key={doctor.id}>
-        <td>{ index+1} </td>
+    <>
+      <Container className="content">
+        <div className="row">
+          <div className="col-sm-12">
+            <h2 className="mt-4 mb-4 fw-bold">
+              Delete a Doctor
+            </h2>
+            <form className="form w-100">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Sr. No</th>
+                    <th>Doctor Name</th>
+                    <th>City</th>
+                    <th>speciality</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {doctors.map((doctor, index) => (
+                    <tr key={doctor.id}>
+                      <td>
+                        { index + 1}
+                        {' '}
+                      </td>
 
-      <td>{ doctor.name} </td>
-      <td>{ doctor.city} </td>
-      <td>{ doctor.speciality} </td>
-      <td><button type="button" onClick={() => handleDelete(doctor.id)}  className="btn btn-danger mb-3">
-        Delete
-      </button></td>
-      </tr>
-        ))
-      }
+                      <td>
+                        { doctor.name}
+                        {' '}
+                      </td>
+                      <td>
+                        { doctor.city}
+                        {' '}
+                      </td>
+                      <td>
+                        { doctor.speciality}
+                        {' '}
+                      </td>
+                      <td>
+                        <button type="button" onClick={() => handleDelete(doctor.id)} className="btn btn-danger mb-3">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
 
-      </tbody>
-      </table>
-      </form>
-      </div>
-      </div>
+                </tbody>
+              </table>
+            </form>
+          </div>
+        </div>
       </Container>
-  </React.Fragment>
+    </>
   );
 };
 
