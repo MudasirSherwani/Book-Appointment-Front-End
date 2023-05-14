3373abb7842b1313e54e8b33d386f231f9a09cac
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 00:25:16 +0300

    Create DeleteDoctor file and imported the dependancies

---
+++ src/components/doctor/DeleteDoctor.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Container } from 'react-bootstrap';
+import { removeDoctor } from "../../redux/doctor/doctorSlice";
+import baseUrl from '../../redux/base_url';
