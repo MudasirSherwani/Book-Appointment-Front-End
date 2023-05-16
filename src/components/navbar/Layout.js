42f3d9f0c550c95bb4d9c0d0bb1d5a687ac839b2
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-16 19:28:31 +0100

    Attempt hamburger for mobile

--- src/components/navbar/Layout.js
+++ src/components/navbar/Layout.js
@@ -2,7 +2,7 @@ import { FaSignOutAlt } from 'react-icons/fa';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
-// import { CgMenuBoxed } from 'react-icons/cg';
+import { CgMenuBoxed } from 'react-icons/cg';
 import { Logo } from '../home/LandingPage';
 import Button from '../button';
 import { logout } from '../../redux/user/auth';
@@ -19,8 +19,8 @@ const Layout = () => {
     <header className="v--header flex column">
       <div className="v--header--content flex w-100 column">
         <img src={Logo} alt="logo" className="main--logo logo" />
-        {/* <CgMenuBoxed className="menu--icon" /> */}
-        <nav className="flex nav--list column hide--menu">
+        <CgMenuBoxed className="menu--icon hide--desktop" />
+        <nav className="flex nav--list column hide--mobile">
           <NavLink to="/" exact activeClassName="active" className="nav--link">Home</NavLink>
           <NavLink to="/book-appointment" activeClassName="active" className="nav--link">New Appointment</NavLink>
           {' '}
@@ -30,7 +30,7 @@ const Layout = () => {
           <Button title="Sign Out" event={handleButtonClick} text=<FaSignOutAlt /> />
         </nav>
       </div>
-      <small className="v--footer hidden">
+      <small className="v--footer hide--mobile">
         &copy; World Vision Clinic 2023
       </small>
       <Outlet />
