79f3dc86618e35e7b73253160b761f7670e5e9c3
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-18 13:18:04 +0100

    Render exit and menu icon conditionally

--- src/components/navbar/Layout.js
+++ src/components/navbar/Layout.js
@@ -17,22 +17,29 @@ const Layout = () => {
   };
 
   // Toggle Logic for menu button
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const [toggle, setToggle] = useState(false);
 
   const handleMenuOpen = () => {
-    setIsMenuOpen(!isMenuOpen);
+    // setIsMenuOpen(!isMenuOpen);
+    setToggle(!toggle);
   };
 
   const handleMenuClose = () => {
-    setIsMenuOpen(false);
+    setToggle(false);
   };
 
   return (
     <header className="v--header flex column">
-      <div className={`v--header--content flex w-100 column ${isMenuOpen ? 'open' : ''}`}>
+      <div className={`v--header--content flex w-100 column ${toggle ? 'open' : ''}`}>
         <img src={Logo} alt="logo" className="main--logo logo" />
-        <CgMenuBoxed className="menu--icon hide--desktop" onClick={handleMenuOpen} />
-        <nav className="flex nav--list column hide--mobile">
+        {
+          toggle
+            ? <AiOutlineCloseSquare className="exit-btn hide--desktop" onClick={handleMenuClose} />
+            : <CgMenuBoxed className="menu--icon hide--desktop" onClick={handleMenuOpen} />
+        }
+        <nav className={`flex nav--list column ${toggle ? '' : 'hide--mobile'}`}>
           <NavLink to="/" exact activeClassName="active" className="nav--link">Home</NavLink>
           <NavLink to="/book-appointment" activeClassName="active" className="nav--link">New Appointment</NavLink>
           {' '}
@@ -40,7 +47,7 @@ const Layout = () => {
           <NavLink to="/delete-doctor" activeClassName="active" className="nav--link">Delete Doctor</NavLink>
           <NavLink to="/reservations/" activeClassName="active" className="nav--link">My Appointments</NavLink>
           <Button title="Sign Out" event={handleButtonClick} text=<FaSignOutAlt /> />
-          <AiOutlineCloseSquare className="menu--icon hide--desktop" onClick={handleMenuClose} />
+          <AiOutlineCloseSquare className="exit-btn hide--desktop" onClick={handleMenuClose} />
         </nav>
       </div>
       <small className="v--footer hide--mobile">
