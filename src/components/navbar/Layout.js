c3a2d77cbb06bf9f12240fb30eca7353e4143ce2
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-18 11:23:10 +0100

    Make loading interface responsive

--- src/components/navbar/Layout.js
+++ src/components/navbar/Layout.js
@@ -1,5 +1,6 @@
+import { AiOutlineCloseSquare } from 'react-icons/ai';
 import { FaSignOutAlt } from 'react-icons/fa';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import { CgMenuBoxed } from 'react-icons/cg';
@@ -15,11 +16,22 @@ const Layout = () => {
     redirect('/');
   };
 
+  // Toggle Logic for menu button
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="v--header flex column">
-      <div className="v--header--content flex w-100 column">
+      <div className={`v--header--content flex w-100 column ${isMenuOpen ? 'open' : ''}`}>
         <img src={Logo} alt="logo" className="main--logo logo" />
-        <CgMenuBoxed className="menu--icon hide--desktop" />
+        <CgMenuBoxed className="menu--icon hide--desktop" onClick={handleMenuOpen} />
         <nav className="flex nav--list column hide--mobile">
           <NavLink to="/" exact activeClassName="active" className="nav--link">Home</NavLink>
           <NavLink to="/book-appointment" activeClassName="active" className="nav--link">New Appointment</NavLink>
@@ -28,6 +40,7 @@ const Layout = () => {
           <NavLink to="/delete-doctor" activeClassName="active" className="nav--link">Delete Doctor</NavLink>
           <NavLink to="/reservations/" activeClassName="active" className="nav--link">My Appointments</NavLink>
           <Button title="Sign Out" event={handleButtonClick} text=<FaSignOutAlt /> />
+          <AiOutlineCloseSquare className="menu--icon hide--desktop" onClick={handleMenuClose} />
         </nav>
       </div>
       <small className="v--footer hide--mobile">
